23db1a5f348f441bf066b11050f65542a2ab0449
Matthjass13 <162318650+Matthjass13@users.noreply.github.com>
2026-09-21 13:27:28 +0200

    Update modele.php

--- modele/modele.php
+++ modele/modele.php
@@ -3,8 +3,8 @@
 function connect_bd() {
   $servername = "3b2wc.myd.infomaniak.com";
   $username = "3b2wc_webmaster";
-  $password = "Layton4Smash!";
-  $bdd = "3b2wc_cinematrice";
+  $password = "";
+  $bdd = "";
   try {
       $connect = new PDO("mysql:host=$servername;dbname=$bdd", $username, $password);
       $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
